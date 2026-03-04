7b951a3270183d1bd135c891b677d9e11ed0d6bb
xavierjr10 <xulloa3716@uta.edu.ec>
2026-03-03 20:10:15 -0500

    first commit

---
+++ CleanTeeth.Domain/Entities/Patient.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanTeeth.Domain.Entities;
+
+public class Patient
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; } = null;
+    public string Email { get; private set; } = null;
+}
